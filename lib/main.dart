1ccad0710aa466f3ee473def4e3cb975e8918cff
Andre Moraes <andrelsmoraes@gmail.com>
2018-06-20 02:05:46 -0300

    Using BottomNavigationDrawer instead of Backdrop menu.

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,179 @@
 import 'package:flutter/material.dart';
-import 'backdrop.dart';
-import 'menu.dart';
+
 import 'phrase_listing.dart';
 
 void main() => runApp(ThreeHundredApp());
 
+// https://github.com/fablue/building-a-social-network-with-flutter
 class ThreeHundredApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
   _ThreeHundredApp createState() => new _ThreeHundredApp();
 }
 
-class _ThreeHundredApp extends State<ThreeHundredApp> {
-  MenuCategory _currentCategory = MenuCategory.all;
-  String _frontSubTitle = "All";
+class _ThreeHundredApp extends State<ThreeHundredApp>
+    with SingleTickerProviderStateMixin{
+  PageController _pageController;
+  int _currentPage = 0;
+  bool _showSearch = true;
 
-  void _onCategoryTap(MenuCategory category) {
-    setState(() {
-      _currentCategory = category;
-
-      if (_currentCategory == MenuCategory.all) {
-        _frontSubTitle = "All";
-      } else if (_currentCategory == MenuCategory.favorites) {
-        _frontSubTitle = "Favorites";
-      } else {
-        _frontSubTitle = "About";
-      }
-    });
-  }
+  Color _appBarColor = Colors.red;
+  IconButton _appBarLeadingIcon;
+  Widget _appBarTitle = new Text(
+    "300 English",
+    style: new TextStyle(color: Colors.white),
+  );
+  Icon _appBarSearchIcon = new Icon(Icons.search, color: Colors.white);
+  TextEditingController _searchController;
+  String _searchText;
 
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "300 English",
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.red,
       ),
-      home: Backdrop(
-        currentCategory: MenuCategory.all,
-        frontLayer: PhraseListing(),
-        backLayer: Menu(
-          currentCategory: _currentCategory,
-          onCategoryTap: _onCategoryTap,
+      home: new Scaffold(
+        appBar: new AppBar(
+            backgroundColor: _appBarColor,
+            title: _appBarTitle,
+            leading: _appBarLeadingIcon,
+            actions: getAppBarActions(context)
         ),
-        frontTitle: Text("300 English"),
-        frontSubTitle: _frontSubTitle,
-        backTitle: Text("Menu"),
-      )
+        body: new PageView(
+            children: [
+              new PhraseListing(searchText: _searchText),
+              new Container(color: Colors.green),
+              new Container(color: Colors.grey)
+            ],
+            controller: _pageController,
+            onPageChanged: onPageChanged
+        ),
+        bottomNavigationBar: new BottomNavigationBar(
+            items: [
+              new BottomNavigationBarItem(
+                  icon: new Icon(Icons.library_books),
+                  title: new Text("All")
+              ),
+              new BottomNavigationBarItem(
+                  icon: new Icon(Icons.favorite),
+                  title: new Text("Favorites")
+              ),
+              new BottomNavigationBarItem(
+                  icon: new Icon(Icons.info),
+                  title: new Text("About")
+              ),
+            ],
+            onTap: onNavigationTapped,
+            currentIndex: _currentPage
+        ),
+      ),
     );
   }
+
+  List<Widget> getAppBarActions(BuildContext context) {
+    if (_showSearch) {
+      return <Widget>[
+        new IconButton(
+          icon: _appBarSearchIcon,
+          onPressed: () {
+            setState(() {
+              if (this._appBarSearchIcon.icon == Icons.search) {
+                this._appBarColor = Colors.grey[200];
+                this._appBarLeadingIcon = new IconButton(
+                  icon: new Icon(Icons.arrow_back),
+                  color: Colors.grey[800],
+                  onPressed: (){
+                    onSearchFinished();
+                  },
+                );
+                this._appBarSearchIcon = new Icon(
+                  Icons.close,
+                  color: Colors.grey[800],
+                );
+                this._appBarTitle = new Theme(
+                  // https://stackoverflow.com/questions/48706884/change-textfields-underline-in-flutter
+                    data: Theme
+                        .of(context)
+                        .copyWith(primaryColor: Colors.transparent),
+                    child: new TextField(
+                      autofocus: true,
+                      controller: _searchController,
+                      style: new TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.grey[800],
+                      ),
+                      decoration: new InputDecoration(
+                          hintText: "Filter by word...",
+                          hintStyle: new TextStyle(color: Colors.grey[800]),
+                          labelStyle: Theme
+                              .of(context)
+                              .textTheme
+                              .caption
+                              .copyWith(color: Colors.transparent)),
+                    ));
+                onSearchStarted();
+              } else {
+                _searchController.clear();
+              }
+            });
+          },
+        ),
+      ];
+    } else {
+      return <Widget>[];
+    }
+  }
+
+  void onSearchStarted() {}
+
+  void onSearchFinished() {
+    setState(() {
+      this._appBarColor = Colors.red;
+      this._appBarLeadingIcon = null;
+      this._appBarSearchIcon = new Icon(
+        Icons.search,
+        color: Colors.white,
+      );
+      this._appBarTitle = new Text(
+        "300 English",
+        style: new TextStyle(color: Colors.white),
+      );
+      _searchController.clear();
+    });
+  }
+
+  void onNavigationTapped(int page) {
+    // Animating to the page.
+    // You can use whatever duration and curve you like
+    _pageController.animateToPage(page,
+        duration: const Duration(milliseconds: 300), curve: Curves.ease);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      this._currentPage = page;
+      this._showSearch = page < 2;
+      onSearchFinished();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = new PageController();
+    _searchController = new TextEditingController();
+    _searchController.addListener((){
+      setState(() {
+        _searchText = _searchController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
 }
--- lib/phrase_listing.dart
+++ lib/phrase_listing.dart
@@ -2,46 +2,19 @@ import 'package:flutter/material.dart';
 import 'phrase_details.dart';
 
 class PhraseListing extends StatefulWidget {
-  PhraseListing({Key key}) : super(key: key);
+  final String searchText;
+
+  PhraseListing({Key key, this.searchText}) : super(key: key);
   @override
   _SearchListState createState() => new _SearchListState();
 }
 
 class _SearchListState extends State<PhraseListing> {
-  Widget appBarTitle = new Text(
-    "300 English",
-    style: new TextStyle(color: Colors.white),
-  );
-  Icon actionIcon = new Icon(
-    Icons.search,
-    color: Colors.white,
-  );
-  final key = new GlobalKey<ScaffoldState>();
-  final TextEditingController _searchQuery = new TextEditingController();
   List<String> _list;
-  bool _isSearching;
-  String _searchText = "";
-
-  _SearchListState() {
-    _searchQuery.addListener(() {
-      if (_searchQuery.text.isEmpty) {
-        setState(() {
-          _isSearching = false;
-          _searchText = "";
-        });
-      } else {
-        setState(() {
-          _isSearching = true;
-          _searchText = _searchQuery.text;
-        });
-      }
-    });
-  }
 
   @override
   void initState() {
     super.initState();
-    _isSearching = false;
     init();
   }
 
@@ -63,11 +36,9 @@ class _SearchListState extends State<PhraseListing> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      key: key,
-      //appBar: buildBar(context),
       body: new ListView(
-        padding: new EdgeInsets.symmetric(vertical: 8.0),
-        children: _isSearching ? _buildSearchList() : _buildList(),
+          padding: new EdgeInsets.symmetric(vertical: 8.0),
+          children: _buildSearchList(widget.searchText)
       ),
     );
   }
@@ -76,14 +47,15 @@ class _SearchListState extends State<PhraseListing> {
     return _list.map((contact) => new ChildItem(contact)).toList();
   }
 
-  List<ChildItem> _buildSearchList() {
-    if (_searchText.isEmpty) {
-      return _list.map((contact) => new ChildItem(contact)).toList();
+  List<ChildItem> _buildSearchList(String searchText) {
+    if (searchText == null || searchText.isEmpty) {
+      return _buildList();
+
     } else {
       List<String> _searchList = List();
       for (int i = 0; i < _list.length; i++) {
         String name = _list.elementAt(i);
-        if (name.toLowerCase().contains(_searchText.toLowerCase())) {
+        if (name.toLowerCase().contains(searchText.toLowerCase())) {
           _searchList.add(name);
         }
       }
@@ -91,58 +63,6 @@ class _SearchListState extends State<PhraseListing> {
     }
   }
 
-  Widget buildBar(BuildContext context) {
-    return new AppBar(title: appBarTitle, actions: <Widget>[
-      new IconButton(
-        icon: actionIcon,
-        onPressed: () {
-          setState(() {
-            if (this.actionIcon.icon == Icons.search) {
-              this.actionIcon = new Icon(
-                Icons.close,
-                color: Colors.white,
-              );
-              this.appBarTitle = new TextField(
-                autofocus: true,
-                controller: _searchQuery,
-                style: new TextStyle(
-                  color: Colors.white,
-                ),
-                decoration: new InputDecoration(
-                    prefixIcon: new Icon(Icons.search, color: Colors.white),
-                    hintText: "Search for word or context...",
-                    hintStyle: new TextStyle(color: Colors.white)),
-              );
-              _handleSearchStart();
-            } else {
-              _handleSearchEnd();
-            }
-          });
-        },
-      ),
-    ]);
-  }
-
-  void _handleSearchStart() {
-    setState(() {
-      _isSearching = true;
-    });
-  }
-
-  void _handleSearchEnd() {
-    setState(() {
-      this.actionIcon = new Icon(
-        Icons.search,
-        color: Colors.white,
-      );
-      this.appBarTitle = new Text(
-        "300 English",
-        style: new TextStyle(color: Colors.white),
-      );
-      _isSearching = false;
-      _searchQuery.clear();
-    });
-  }
 }
 
 class ChildItem extends StatelessWidget {
@@ -151,13 +71,13 @@ class ChildItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new ListTile(
-      title: new Text(this.name),
-      onTap: () {
-        Navigator.push(
-          context,
-          new MaterialPageRoute(builder: (context) => new PhraseDetails(phrase: this.name,)),
-        );
-      }
+        title: new Text(this.name),
+        onTap: () {
+          Navigator.push(
+            context,
+            new MaterialPageRoute(builder: (context) => new PhraseDetails(phrase: this.name,)),
+          );
+        }
     );
   }
 }
